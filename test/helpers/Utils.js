025600e28227fc9f0ea447c18faedaed1d2fcfbd
firyal <firyal.fakhril@gmail.com>
2017-08-05 20:01:19 +0700

    test for utils module

---
+++ test/helpers/Utils.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const expect = require('chai').expect
+, proxyquire = require('proxyquire')
+, sinon = require("sinon")
+, Utils = require(`${process.env.PWD}/app/helpers/Utils`)
+
+describe("utils helper accessObjectPropertyByString", function() {
+	var object
+	beforeEach(function () {
+		object = {
+			key: 123
+			, name: 'John Doe'
+			, child: [{
+				key: 1231
+				, name: 'John Rue'
+			}]
+		}
+	})
+
+	it('should returns the object property by name using string', ()=> {
+		var objectProp = Utils.accessObjectPropertyByString(object, 'name')
+		
+		expect(objectProp).to.equal('John Doe')
+	})
+
+	it('should returns null for no string match', ()=> {
+		var objectProp = Utils.accessObjectPropertyByString(object, 'bio')
+		
+		expect(objectProp).to.equal(null)
+	})
+
+	// it('should throw exception for undefined parameters', ()=> {
+	// 	expect(function(){ Utils.accessObjectPropertyByString() }).to.throw(TypeError)
+	// })
+})
+
+describe("utils helper promisedFS", function() {
+
+})
