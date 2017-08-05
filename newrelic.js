70a097d4651f36b96bca02295f7f3270395833f1
firyal <firyal.fakhril@gmail.com>
2017-08-05 17:33:07 +0700

    change config path
    change to new config path, in root of prohect

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const nrConfig = require('./app/configs/'+((process.env.NODE_ENV==='local-dev')?'local-dev/':'')+'newrelic')[process.env.NODE_ENV];
+const nrConfig = require(`${process.env.PWD}/configs/monitoring`)
 
 exports.config = {
   app_name: nrConfig.app_name
