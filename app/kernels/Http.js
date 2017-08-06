41c0d25ece30a410e189a92bbf1c68248b723d41
firyal <firyal.fakhril@gmail.com>
2017-08-07 03:25:48 +0700

    set allowed header middleawre

--- app/kernels/Http.js
+++ app/kernels/Http.js
@@ -7,6 +7,16 @@ module.exports = (app) => {
 		app.use(bodyParser.json());
 		app.use(bodyParser.urlencoded({	extended: false }));
 
+		app.use(function(req, res, next) {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Credentials', 'true');
+			res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+			res.setHeader('Access-Control-Allow-Headers'
+				, 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+			res.setHeader('Cache-Control', 'no-cache');
+			next();
+		});
+
 		return true;
 	}
 	catch(e){
