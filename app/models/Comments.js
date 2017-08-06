d9f69aab289065573ff79d7f9cbb90a410931874
firyal <firyal.fakhril@gmail.com>
2017-08-06 17:07:10 +0700

    sample model

---
+++ app/models/Comments.js
@@ -0,0 +1,9 @@
+'use strict'
+const mongoose = require('mongoose')
+, Schema = mongoose.Schema
+, CommentsSchema = {
+  author: String,
+  text: String
+};
+
+module.exports = mongoose.model('Comments', CommentsSchema);
